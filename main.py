d2e45fc27e7521fc2400463ae0f68e3c688947c8
Suhas Aitham <83656128+suhasaitham22@users.noreply.github.com>
2023-11-22 23:03:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
+import statsmodels.api as sm
 from statsmodels.tsa.arima.model import ARIMA
 import itertools
 import torch
